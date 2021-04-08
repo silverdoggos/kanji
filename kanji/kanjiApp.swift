ccce98cc5776f8d2828269a8c88a41ad523ff674
Artem Shishkin <shishkinart@icloud.com>
2021-04-08 16:41:30 +0300

    Initial Commit

---
+++ kanji/kanjiApp.swift
@@ -0,0 +1,17 @@
+//
+//  kanjiApp.swift
+//  kanji
+//
+//  Created by Artem Shishkin on 08.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct kanjiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
